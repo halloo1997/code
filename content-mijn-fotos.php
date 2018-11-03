ba7a1e0f375b045c64d8578d392f992286653dd2
chariesa <chariesa@hotmail.com>
2018-11-03 16:17:50 +0100

    hoi

--- content-mijn-fotos.php
+++ content-mijn-fotos.php
@@ -5,7 +5,7 @@
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img id="myImg" src="images/MA282834.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img id="myImg" src="images/MA282834.jpg" alt="Vogeltje" />
                         </a>
                     </div>
                 </li>
@@ -13,182 +13,182 @@
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/MA9042782.jpg" alt="" style="width:100%;max-width:300px" />
+                            <img src="images/MA9042782.jpg" alt=""  />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/MAI000004.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/MAI000004.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/MAI000026.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/MAI000026.jpg" alt=""/>
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/P4260567.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/P4260567.jpg" alt=""/>
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink" >
-                            <img src="images/P4260570.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/P4260570.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/P4260584.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/P4260584.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/P4270624.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/P4270624.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/P4270648.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/P4270648.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/P4280685.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/P4280685.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink" >
-                            <img  src="images/PA090042.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/PA090042.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink" >
-                            <img  src="images/SLindex1.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/SLindex1.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/ST6140031.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/ST6140031.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/ST6140063.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/ST6140063.jpg" alt=""/>
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/ST6140108.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/ST6140108.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/ST6150112.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/ST6150112.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/ST6150128.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/ST6150128.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/ST6150134.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/ST6150134.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/ST6150214.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/ST6150214.jpg" alt=""/>
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/ST6170444.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/ST6170444.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/ST6170448.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/ST6170448.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img  src="images/TSMG_4817.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img  src="images/TSMG_4817.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink"">
-                            <img src="images/TSMG_4964.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/TSMG_4964.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/UTG9012765.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/UTG9012765.jpg" alt=""/>
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/VU9210077.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/VU9210077.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/WLindex.jpg" alt="" style="width:100%;max-width:300px"/>
+                            <img src="images/WLindex.jpg" alt="" />
                         </a>
                     </div>
                 </li>
                 <li class="hex">
                     <div class="hexIn">
                         <a class="hexLink">
-                            <img src="images/P9221055.jpg" alt="" style="width:100%;max-width:300px" />
+                            <img src="images/P9221055.jpg" alt="" />
                         </a>
                     </div>
                 </li>
