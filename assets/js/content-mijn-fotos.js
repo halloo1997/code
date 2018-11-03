97cec29144925027b2d7aa8e41376caed3608916
chariesa <chariesa@hotmail.com>
2018-11-03 16:13:46 +0100

    hoi

--- assets/js/content-mijn-fotos.js
+++ assets/js/content-mijn-fotos.js
@@ -5,13 +5,11 @@ var modal = document.getElementById('myModal');
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
-var modalImg1 = document.getElementById("img02");
 
 var captionText = document.getElementById("caption");
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
-    modalImg1.src = this.src;
     captionText.innerHTML = this.alt;
 }
 
